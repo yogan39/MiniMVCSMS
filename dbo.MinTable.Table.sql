2bfaf90a320d22be8a1f8e843e83ba4f6a16e94a
yogan39 <yogan39@gmail.com>
2017-07-07 13:16:01 +0530

    Update dbo.MinTable.Table.sql

--- dbo.MinTable.Table.sql
+++ dbo.MinTable.Table.sql
@@ -1,12 +1,3 @@
-IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[MinTable]') AND type in (N'U'))
-DROP TABLE [dbo].[MinTable]
-GO
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[MinTable]') AND type in (N'U'))
-BEGIN
 CREATE TABLE [dbo].[MinTable](
 	[Id] [int] IDENTITY(1,1) NOT NULL,
 	[MobileNos] [nvarchar](max) NULL,
@@ -18,5 +9,4 @@ PRIMARY KEY CLUSTERED
 	[Id] ASC
 )WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
 ) ON [PRIMARY]
-END
-GO
+
